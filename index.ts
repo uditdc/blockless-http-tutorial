120eae3aebbfbd53f11706f78ef1802f6ac52ea6
Udit <25996904+uditdc@users.noreply.github.com>
2023-07-11 01:22:26 +0530

    Try trigger type request body

--- index.ts
+++ index.ts
@@ -1,11 +1,30 @@
-import { http } from "@blockless/sdk"
+import { http, json } from "@blockless/sdk"
 
 /**
  * Fetch sample JSON content and serve.
  * 
  */
 http.HttpComponent.serve((req: http.Request) => {
-  const response = new http.Client().get('https://httpbin.org/json')
+  const response = 'Trigger Response'
+
+  const bodyStr = req.body
+  const body = json.JSON.parse(bodyStr)
+
+  // 1. Sample Webhook Event
+  // Data passed through from the POST /webhook/:id API 
+  // bodyStr === JSON.stringify(request.body)
+
+  // 2. Sample Request EVM Event
+  // Data passed in from the event listener
+  // {
+  //   chainId: number,
+  //   blockNumber: number,
+  //   blockHash: string,
+  //   transactionHash: string,
+  //   address: string,
+  //   event: string,
+  //   args: unknown
+  // }
 
   return new http.Response(response.stringify())
     .header('Content-Type', 'application/json')
